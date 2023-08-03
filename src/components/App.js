9670e159ff81ee79a4f419c881348b2297626593
AmanVerma5 <amanv4775@gmail.com>
2023-08-03 16:49:36 +0530

    submitting code-Thu Aug 03 2023 16:49:35 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -24,26 +24,26 @@ const App=()=>{
     }
 
     return (
-        <div className="calculator">
+        <div className="Calculator">
            <input type="text" className="output" value={number} />
            <div className="grid">
-            <div onClick={handleClick}>C</div>
-            <div onClick={handleClick}>/</div>
-            <div onClick={handleClick}>*</div>
-            <div onClick={handleClick}>-</div>
-            <div onClick={handleClick}>7</div>
-            <div onClick={handleClick}>8</div>
-            <div onClick={handleClick}>9</div>
-            <button onClick={handleClick} className="add">+</button>
-            <div onClick={handleClick}>4</div>
-            <div onClick={handleClick}>5</div>
-            <div onClick={handleClick}>6</div>
-            <div onClick={handleClick}>1</div>
-            <div onClick={handleClick}>2</div>
-            <div onClick={handleClick}>3</div>
-            <button onClick={handleClick} className="equals">=</button>
-            <div onClick={handleClick}>0</div>
-            <div  onClick={handleClick} className="dot">.</div>
+            <div id='btn-C' onClick={handleClick}>C</div>
+            <div id='btn-/'  onClick={handleClick}>/</div>
+            <div id='btn-*'  onClick={handleClick}>*</div>
+            <div id='btn--'  onClick={handleClick}>-</div>
+            <div id='btn-7'  onClick={handleClick}>7</div>
+            <div id='btn-8'  onClick={handleClick}>8</div>
+            <div id='btn-9'  onClick={handleClick}>9</div>
+            <button id='btn-+'   onClick={handleClick} className="add">+</button>
+            <div id='btn-4'  onClick={handleClick}>4</div>
+            <div id='btn-5'  onClick={handleClick}>5</div>
+            <div id='btn-6'  onClick={handleClick}>6</div>
+            <div id='btn-1'  onClick={handleClick}>1</div>
+            <div id='btn-2'  onClick={handleClick}>2</div>
+            <div id='btn-3'  onClick={handleClick}>3</div>
+            <button id='btn-='  onClick={handleClick} className="equals">=</button>
+            <div  id='btn-0'  onClick={handleClick}>0</div>
+            <div id='btn-.'   onClick={handleClick} className="dot">.</div>
            </div>
         </div>
     )
